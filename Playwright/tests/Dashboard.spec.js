4734c9c1fb827e4ab2cb177c5971f6d3f7f54c99
Emarson-s <emarson.s@mavens-i.com>
2024-08-27 11:25:50 +0530

    update dashboard

--- Playwright/tests/Dashboard.spec.js
+++ Playwright/tests/Dashboard.spec.js
@@ -2,6 +2,12 @@ const {test, expect} =require('@playwright/test');
 
 test('Dashboard', async({page})=> {
 
+await page.goto('https://awsdev.iksana.life/#/en/login');
+
+await page.fill("input[name='username']",'Joseph@yopmail.com');
     
+await page.fill("input[placeholder='Enter password']", 'Iksana@123');
+    
+await page.click("//button[text()='Sign in']");
 
 })
