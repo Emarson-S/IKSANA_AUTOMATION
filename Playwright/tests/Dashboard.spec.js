41f7c29905980205a6448dff0efdd54434acfd2d
Emarson-s <emarson.s@mavens-i.com>
2024-08-28 15:08:05 +0530

    add files foldar

--- Playwright/tests/Dashboard.spec.js
+++ Playwright/tests/Dashboard.spec.js
@@ -2,6 +2,7 @@ const {test, expect} =require('@playwright/test');
 
 test('Dashboard', async({page})=> {
 
+
 await page.goto('https://awsdev.iksana.life/#/en/login');
 
 await page.fill("input[name='username']",'jeevabalin.r@mavens-i.com');
@@ -10,8 +11,9 @@ await page.fill("input[placeholder='Enter password']", 'Emarson@123');
     
 await page.click("//button[text()='Sign in']");
 
-const text = await page.getByText("//small[@class='fs-10 mb-0 text-uppercase text-mute']");
+const text =await page.locator("(//small[@class='fs-10 mb-0 text-uppercase text-mute'])[1]").textContent();
 
-console.log(text);
+await expect(await page.getByText(text)).toBeVisible();
+
 
 })
